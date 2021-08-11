dd28a283dae8645c427e01fce94c484d10280008
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-11 23:11:40 +0430

    feat: add TodosContext as a context
    This context is used as a general state for other components that needs that state

---
+++ src/context/TodosContexts.jsx
@@ -0,0 +1,11 @@
+import React, { createContext, useState } from "react";
+export const TodosContext = createContext();
+
+export default function TodosProvider({ children }) {
+  const [todos, setTodos] = useState([]);
+  return (
+    <TodosContext.Provider value={{ todos, setTodos }}>
+      {children}
+    </TodosContext.Provider>
+  );
+}
