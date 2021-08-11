ffdc1ffa0e1fc097ce67b54d5a693048bf5a6b03
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-11 23:19:13 +0430

    feat: add handleAdd function
    This function changes todos state that imported from TodosContext, this will be done via useContext

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { TodosContext } from "../context/TodosContexts";
+import { v4 as uuidv4 } from "uuid";
+
 const InputForm = () => {
+  const { todos, setTodos } = useContext(TodosContext);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAdd = () => {
+    const newTodos = [...todos];
+    const newTodoObj = {
+      id: uuidv4(),
+      title: inputValue,
+      isCompleted: false,
+    };
+    newTodos.unshift(newTodoObj);
+    setTodos(newTodos);
+    setInputValue("");
+  };
+
   return (
     <div className="add-items d-flex">
       <input
         type="text"
         className="form-control todo-list-input"
         placeholder="What do you need to do today?"
+        onChange={(e) => setInputValue(e.currentTarget.value)}
+        value={inputValue}
       />
-      <button className="add btn btn-primary font-weight-bold todo-list-add-btn">
+      <button
+        className="add btn btn-primary font-weight-bold todo-list-add-btn"
+        onClick={handleAdd}
+      >
         Add
       </button>
     </div>
